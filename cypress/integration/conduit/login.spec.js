66e5b3c8b64a71e3c03b25a8cafbb51a5c8e2581
muthash <kathingo92@gmail.com>
2020-06-18 21:32:21 +0300

    Install cypress

---
+++ cypress/integration/conduit/login.spec.js
@@ -0,0 +1,9 @@
+describe('Login', () => {
+    it('Sign in', () => {
+        cy.visit('https://react-redux.realworld.io')
+        cy.get('.navbar').contains('Sign in').click()
+        cy.get('input[type=email]').type('muthash@github.com')
+        cy.get('input[type=password]').type('muthash123')
+        cy.get('.btn').contains('Sign in').should('be.visible').click()
+    })
+})
